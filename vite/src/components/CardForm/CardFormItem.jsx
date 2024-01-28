4e8be8494374c5aacbcbccd8961c341fc2d588f4
Illia Hryn <grinpis01@gmail.com>
2024-01-28 12:01:17 +0200

    strokeWidth

--- vite/src/components/CardForm/CardFormItem.jsx
+++ vite/src/components/CardForm/CardFormItem.jsx
@@ -37,13 +37,13 @@ function CardForm({ statusTask, taskType, date }) {
                                     <path
                                         d="M10.5625 2.03125H2.4375C1.7644 2.03125 1.21875 2.5769 1.21875 3.25V10.5625C1.21875 11.2356 1.7644 11.7812 2.4375 11.7812H10.5625C11.2356 11.7812 11.7812 11.2356 11.7812 10.5625V3.25C11.7812 2.5769 11.2356 2.03125 10.5625 2.03125Z"
                                         stroke="#94A6BE"
-                                        stroke-width="0.8"
+                                        strokeWidth="0.8"
                                         stroke-linejoin="round"
                                     />
                                     <path
                                         d="M11.7812 4.0625H1.21875M3.25 1.21875V2.03125V1.21875ZM9.75 1.21875V2.03125V1.21875Z"
                                         stroke="#94A6BE"
-                                        stroke-width="0.8"
+                                        strokeWidth="0.8"
                                         stroke-linecap="round"
                                         stroke-linejoin="round"
                                     />
