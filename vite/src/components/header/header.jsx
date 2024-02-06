c17eb44efd8df0581d4e5e4b9d280fcb32880b8d
Illia Hryn <grinpis01@gmail.com>
2024-02-06 09:27:40 +0200

    dropMenu

--- vite/src/components/header/header.jsx
+++ vite/src/components/header/header.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 function MainHeader() {
+    const [isOpen, setIsOpen] = useState(false);
+    const isOpenMenu = () => {
+        setIsOpen((prevState) => !prevState);
+    };
+
     return (
         <header className="header">
             <div className="container">
@@ -32,34 +39,38 @@ function MainHeader() {
                         >
                             <a href="#popNewCard">Создать новую задачу</a>
                         </button>
+
                         <a
                             href="#user-set-target"
                             className="header__user _hover02"
+                            onClick={isOpenMenu}
                         >
                             Ivan Ivanov
                         </a>
-                        <div
-                            className="header__pop-user-set pop-user-set"
-                            id="user-set-target"
-                        >
-                            {/* <!-- <a href="">x</a> --> */}
-                            <p className="pop-user-set__name">Ivan Ivanov</p>
-                            <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-                            <div className="pop-user-set__theme">
-                                <p>Темная тема</p>
-                                <input
-                                    type="checkbox"
-                                    className="checkbox"
-                                    name="checkbox"
-                                />
-                            </div>
-                            <button
-                                type="button"
-                                className="_hover03"
+                        {isOpen && (
+                            <div
+                                className="header__pop-user-set pop-user-set"
+                                id="user-set-target"
                             >
-                                <a href="#popExit">Выйти</a>
-                            </button>
-                        </div>
+                                {/* <!-- <a href="">x</a> --> */}
+                                <p className="pop-user-set__name">Ivan Ivanov</p>
+                                <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
+                                <div className="pop-user-set__theme">
+                                    <p>Темная тема</p>
+                                    <input
+                                        type="checkbox"
+                                        className="checkbox"
+                                        name="checkbox"
+                                    />
+                                </div>
+                                <button
+                                    type="button"
+                                    className="_hover03"
+                                >
+                                    <a href="#popExit">Выйти</a>
+                                </button>
+                            </div>
+                        )}
                     </nav>
                 </div>
             </div>
---
+++ vite/src/data.js
@@ -0,0 +1,16 @@
+export const cardList = [
+    {
+        id: 1,
+        theme: "Web Design",
+        title: "Название задачи",
+        date: "09.09.23",
+        status: "Нужно сделать",
+    },
+    {
+        id: 2,
+        theme: "Research",
+        title: "Новая задача",
+        date: "09.09.23",
+        status: "Нужно сделать",
+    },
+];
