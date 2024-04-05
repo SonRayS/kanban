b72d09ed1beb18ea38aa8baf237d7c7ead7455a5
Illia Hryn <grinpis01@gmail.com>
2024-04-06 02:23:58 +0300

    TaskProvider

--- vite/src/main.jsx
+++ vite/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./contexts/userLocalStorage.jsx";
+import { TaskProvider } from "./contexts/tasks.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
         <UserProvider>
-            <App />
+            <TaskProvider>
+                <App />
+            </TaskProvider>
         </UserProvider>
     </BrowserRouter>
 );
