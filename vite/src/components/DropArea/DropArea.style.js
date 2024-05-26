9702e28e09234f4f8f62284928eefa154cebe956
Illia Hryn <grinpis01@gmail.com>
2024-05-26 17:34:09 +0200

    style DnD

--- vite/src/components/DropArea/DropArea.style.js
+++ vite/src/components/DropArea/DropArea.style.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export const statusDrop = {
     true: `
-    
+        width: 220px;
+        height: 130px;
         border-radius: 10px;
         display:flex;
         align-content: center;
@@ -11,10 +12,10 @@ export const statusDrop = {
         opacity: 1;
         color: #FFFFFF;
         margin-bottom: 8px;
-        margin-top: 8px;
+        margin-top: 20px;
         font-size: 23px;
         border: solid #b8bbec 1px;
-        padding: 15px
+        padding: 20px
         columns: #FFFFFF
         transition: all 0.3s ease-in-out;
         `,
